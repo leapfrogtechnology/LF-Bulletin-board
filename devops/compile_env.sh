87163ddd2a565c686a5fd5b6689dbb6e0791e195
Pravash Upreti <pravashupreti@lftechnology.com>
2019-01-17 10:43:15 +0545

    log the compiled .env

--- devops/compile_env.sh
+++ devops/compile_env.sh
@@ -3,3 +3,5 @@ apk add curl && apk update
 
 echo  "X-Vault-Token:$VAULT_TOKEN" $VAULT_SERVER/v1/secret/$VAULT_SECRET_DIR/$STAGE/app 
 curl --header "X-Vault-Token:$VAULT_TOKEN" $VAULT_SERVER/v1/secret/$VAULT_SECRET_DIR/$STAGE/app | jq .data | jq -r "to_entries|map(\"\(.key)=\(.value|tostring)\")|.[]" > ../app/.env
+echo "Compiled .env file for $STAGE environment is"
+cat ../app/.env
