408b406351ffaa3180bbd364a1db0136c912fccb
Pravash Upreti <pravashupreti@lftechnology.com>
2019-01-17 10:33:51 +0545

    Enable compiling .env in ci

---
+++ devops/compile_env.sh
@@ -0,0 +1,5 @@
+#! /bin/sh
+apk add curl && apk update
+
+echo  "X-Vault-Token:$VAULT_TOKEN" $VAULT_SERVER/v1/secret/$VAULT_SECRET_DIR/$STAGE/app 
+curl --header "X-Vault-Token:$VAULT_TOKEN" $VAULT_SERVER/v1/secret/$VAULT_SECRET_DIR/$STAGE/app | jq .data | jq -r "to_entries|map(\"\(.key)=\(.value|tostring)\")|.[]" > .env
