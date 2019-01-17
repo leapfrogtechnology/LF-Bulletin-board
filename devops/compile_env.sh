df69802ce74bf6a4bdc163a96460a29e86c5bfd2
Pravash Upreti <pravashupreti@lftechnology.com>
2019-01-17 10:41:44 +0545

    Assign execution permission

--- devops/compile_env.sh
+++ devops/compile_env.sh
@@ -2,4 +2,4 @@
 apk add curl && apk update
 
 echo  "X-Vault-Token:$VAULT_TOKEN" $VAULT_SERVER/v1/secret/$VAULT_SECRET_DIR/$STAGE/app 
-curl --header "X-Vault-Token:$VAULT_TOKEN" $VAULT_SERVER/v1/secret/$VAULT_SECRET_DIR/$STAGE/app | jq .data | jq -r "to_entries|map(\"\(.key)=\(.value|tostring)\")|.[]" > .env
+curl --header "X-Vault-Token:$VAULT_TOKEN" $VAULT_SERVER/v1/secret/$VAULT_SECRET_DIR/$STAGE/app | jq .data | jq -r "to_entries|map(\"\(.key)=\(.value|tostring)\")|.[]" > ../app/.env
