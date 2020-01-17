117a02c0d5153f0c1ba2d837d839fc8b7ae7b166
Biplap Bhattarai <bhattaraib58@gmail.com>
2020-01-17 13:15:52 +0545

    LE-178: Place all modal style in seperate file and add background color as dark

---
+++ app/src/assets/modalStyle.css.js
@@ -0,0 +1,15 @@
+const modalStyle = {
+  overlay: {
+    backgroundColor: 'rgba(0, 0, 0, 0.75)'
+  },
+  content: {
+    top: '50%',
+    left: '50%',
+    right: 'auto',
+    bottom: 'auto',
+    marginRight: '-50%',
+    transform: 'translate(-50%, -50%)'
+  }
+};
+
+export default modalStyle;
