624b09577772a4e95f1143980274d8bc9813bdcf
Biplap Bhattarai <bhattaraib58@gmail.com>
2020-01-17 13:24:30 +0545

    LE-178: Add link for navigating between dashboard and admin

--- app/src/components/dashboard/SideMenu.js
+++ app/src/components/dashboard/SideMenu.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 import MoreMenu from '../moreMenu';
 import logo from '../../assets/images/bulletin-logo-inverse.png';
@@ -23,6 +24,17 @@ class SideMenu extends Component {
         <div className="sidemenu-bulletin-logo">
           <img src={logo} alt="bulletin logo" />
         </div>
+        <Link to="/dashboard/list">
+          <div className="sidemenu-icons">
+            <i className="fa fa-desktop"></i>
+          </div>
+        </Link>
+
+        <Link to="/dashboard/admin">
+          <div className="sidemenu-icons">
+            <i className="fa fa-user-o"></i>
+          </div>
+        </Link>
         <ul className="side-menu-list">
           <li>
             <span className="profile-name">{this.state.userInitials}</span>
