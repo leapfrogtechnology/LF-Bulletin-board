65527e60dc2f1d1630a546b8bd1743598f07ec32
Biplap Bhattarai <bhattaraib58@gmail.com>
2020-01-19 22:18:50 +0545

    LE-241: Concatinate url into same link

--- app/src/services/auth.js
+++ app/src/services/auth.js
@@ -9,7 +9,7 @@ import urlConstants from '../constants/urlConstants';
 export async function checkLogin() {
   const user = JSON.parse(localStorage.getItem('user')) || {};
 
-  const validateUserUrl = urlConstants.apiBaseUrl + '/validateuser' + '?email=' + user.email;
+  const validateUserUrl = `${urlConstants.apiBaseUrl}/validateuser?email=${user.email}`;
   const result = await httpUtil.get(validateUserUrl, {});
 
   return result;
