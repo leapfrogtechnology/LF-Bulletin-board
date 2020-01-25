5d81f6f214ea40b70a83e7abd6a57e9e854ee4c2
Biplap Bhattarai <bhattaraib58@gmail.com>
2020-01-25 21:01:29 +0545

    LE-275: Create Migration for users role

---
+++ api/src/migrations/20200125143159_add_user_role.js
@@ -0,0 +1,30 @@
+/**
+ * @param  {object} knex
+ * @returns {Promise}
+ */
+export function up(knex) {
+  return knex.schema.table('users', table => {
+    table.unique('email');
+    table.string('role').defaultTo('admin');
+    table
+      .string('created_at')
+      .nullable()
+      .defaultTo(null);
+    table
+      .string('updated_at')
+      .nullable()
+      .defaultTo(null);
+  });
+}
+
+/**
+ * @param  {object} knex
+ * @returns {Promise}
+ */
+export function down(knex) {
+  return knex.schema.table('users', table => {
+    table.dropColumn('role');
+    table.dropColumn('created_at');
+    table.dropColumn('updated_at');
+  });
+}
