32d276f5e64abed7078368847f4fad950f813317
Biplap Bhattarai <bhattaraib58@gmail.com>
2020-03-31 23:41:51 +0545

    LE-296: Fix Url constant env API

--- app/src/constants/urlConstants.js
+++ app/src/constants/urlConstants.js
@@ -1,5 +1,5 @@
 const urlConstants = {
-  baseUrl: process.env.REACT_APP_BASE_HREF,
+  baseUrl: process.env.REACT_APP_BASE_API,
   apiBaseUrl: process.env.REACT_APP_BASE_API_URL,
   googleLoginUrl: process.env.REACT_APP_GOOGLE_LOGIN_URL
 };
