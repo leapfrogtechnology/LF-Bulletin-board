10c903b5b04c87be0d1e03e8e7134d2b55eaaf08
Biplap Bhattarai <bhattaraib58@gmail.com>
2020-03-31 23:49:14 +0545

    LE-296: Create getBulletinList in Bulletin Util

--- app/src/utils/bulletinUtil.js
+++ app/src/utils/bulletinUtil.js
@@ -54,3 +54,77 @@ export function removeIframeBackgroundImage() {
   iframeHolderDiv.style.background = 'none';
   iframeHolderDiv.style.backgroundSize = 'none';
 }
+
+const dummyBulletinSegment = {
+  url: '',
+  duration: 0,
+  title: '',
+  index: 0,
+  show: false
+};
+
+/**
+ * Get Bulletin List With First And Second Frame Selected.
+ *
+ * @param {*} [bulletinList=[]]
+ * @returns {Object}
+ */
+export function getBulletinList(bulletinList = []) {
+  const bulletinListLength = bulletinList.length;
+
+  let selectedData = {};
+
+  if (bulletinListLength === 0) {
+    addIframeBackgroundImage();
+
+    selectedData = {
+      firstSelectedLink: dummyBulletinSegment,
+      secondSelectedLink: dummyBulletinSegment,
+      chosenDuration: 0,
+      activeBulletinTitle: 'Leapfrog Bulletin'
+    };
+  } else if (bulletinListLength === 1) {
+    removeIframeBackgroundImage();
+
+    selectedData = {
+      firstSelectedLink: {
+        url: bulletinList[0].url,
+        duration: bulletinList[0].duration,
+        title: bulletinList[0].title,
+        index: 0,
+        show: true
+      },
+      secondSelectedLink: dummyBulletinSegment,
+      chosenDuration: bulletinList[0].duration,
+      activeBulletinTitle: bulletinList[0].title
+    };
+  } else {
+    removeIframeBackgroundImage();
+
+    selectedData = {
+      firstSelectedLink: {
+        url: bulletinList[0].url,
+        duration: bulletinList[0].duration,
+        title: bulletinList[0].title,
+        index: 0,
+        show: true
+      },
+      secondSelectedLink: {
+        url: bulletinList[1].url,
+        duration: bulletinList[1].duration,
+        title: bulletinList[1].title,
+        index: 1,
+        show: false
+      },
+      chosenDuration: bulletinList[0].duration,
+      activeBulletinTitle: bulletinList[0].title
+    };
+  }
+
+  selectedData = {
+    ...selectedData,
+    dataCollection: bulletinList
+  };
+
+  return selectedData;
+}
