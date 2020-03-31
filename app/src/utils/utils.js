2b9b705c6c311352cf05cffdd28f25c5003d020e
Biplap Bhattarai <bhattaraib58@gmail.com>
2020-03-31 23:43:38 +0545

    LE-296: Move getLink function to util

--- app/src/utils/utils.js
+++ app/src/utils/utils.js
@@ -1,3 +1,4 @@
+import regex from '../constants/regex';
 import textConstants from '../constants/textConstants';
 
 /**
@@ -45,4 +46,50 @@ export function getBulletinFetchInterval() {
   }
 
   return fetchInterval * 1000 * 60;
+}
+
+/**
+ * Get link based on visibility.
+ *
+ * @param {*} link
+ * @param {*} visibility
+ * @param {*} startHiddenSlide
+ * @returns
+ */
+export function getLink(link, visibility, startHiddenSlide) {
+  if (!link) {
+    return '';
+  }
+
+  let newLink = link;
+
+  if (visibility || startHiddenSlide) {
+    if (newLink.search('docs.google.com') > -1) {
+      if (newLink.includes('start=false')) {
+        newLink = newLink.replace('start=false', 'start=true');
+      }
+    } else if (newLink.match(regex.YOUTUBE_REGEX) !== null) {
+      if (newLink.includes('autoplay=0')) {
+        newLink = newLink.replace('autoplay=0', 'autoplay=1');
+      }
+      if (newLink.includes('mute=0')) {
+        newLink = newLink.replace('mute=0', 'mute=1');
+      }
+    }
+  } else {
+    if (newLink.search('docs.google.com') > -1) {
+      if (newLink.includes('start=true')) {
+        newLink = newLink.replace('start=true', 'start=false');
+      }
+    } else if (newLink.match(regex.YOUTUBE_REGEX) !== null) {
+      if (newLink.includes('autoplay=1')) {
+        newLink = newLink.replace('autoplay=1', 'autoplay=0');
+      }
+      if (newLink.includes('mute=1')) {
+        newLink = newLink.replace('mute=1', 'mute=0');
+      }
+    }
+  }
+
+  return newLink;
 }
