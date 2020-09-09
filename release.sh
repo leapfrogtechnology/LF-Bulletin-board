cb236d898eea7ef0289dc5c13fffb5ec14488dd4
Biplap Bhattarai <bhattaraib58@gmail.com>
2020-09-09 09:41:04 +0545

    Update release script to take token and have validation

--- release.sh
+++ release.sh
@@ -4,12 +4,34 @@
 
 set -e
 
+# Set color variables
+RED="\033[0;31m"
+GREEN="\033[0;32m"
+YELLOW="\033[1;33m"
+WHITE="\033[1;37m"
+NC="\033[0m" # No Color, Default Terminal COlor
+
+
+# Checks if the command is executed on valid git repository
+if !(git tag > /dev/null 2>&1 && [ $? -eq 0 ]); then
+  echo -e "${RED} This is not a git repository"
+  echo -e "${RED} Please run this command on git repository"
+  echo ""
+
+  exit 1 # Exit script after printing help
+fi
+
+
 changelog() {
   # NOTE: This requires github_changelog_generator to be installed.
   # https://github.com/github-changelog-generator/github-changelog-generator
 
   if [ -z "$NEXT" ]; then
       NEXT="Unreleased"
+  fi
+
+  if [ -z "$TOKEN" ]; then
+      TOKEN=""
   fi
 
   echo "Generating changelog upto version: $NEXT"
@@ -23,7 +45,8 @@ changelog() {
     --exclude-labels=unnecessary,duplicate,question,invalid,wontfix,exclude-from-changelog \
     --user leapfrogtechnology \
     --project bulletin-board \
-    --exclude-tags v1.0.0,2.0.0
+    --exclude-tags v1.0.0,2.0.0 \
+    --token "$TOKEN"
 }
 
 bump() {
@@ -35,6 +58,9 @@ bump() {
   # Update version in the following files
   sed -i "s/\(\"version\":\s*\"\)[^\"]*\(\"\)/\1${VERSION}\2/g" package.json
 
+  # Update Manifest Version
+  sed -i "s/\(\"version\":\s*\"\)[^\"]*\(\"\)/\1${VERSION}\2/g" public/manifest.json
+
   # Generate change log
   changelog
   echo ""
@@ -43,12 +69,24 @@ bump() {
   yarn
 
   # Prepare to commit
-  git add CHANGELOG.md package.json yarn.lock && \
+  git add CHANGELOG.md package.json yarn.lock public/manifest.json && \
     git commit -v --edit -m "${VERSION} Release :tada: :fireworks: :bell:" && \
     git tag "$NEXT" && \
     echo -e "\nRelease tagged $NEXT"
+
+  # Push new tag and commit to origin  
   git push origin HEAD --tags
 }
+
+CURRENT_BRANCH_NAME="$(git symbolic-ref --short HEAD)"
+
+if [ "$CURRENT_BRANCH" != "master" ]; then
+  echo ""
+  echo -e "${RED}Looks like you are not on master branch"
+  echo -e "${RED}Although this Changelog read's commit history from master branch"
+  echo -e "${RED}Reason Being Other Branch will not have changelog and version history"
+  echo -e "${NC}"
+fi
 
 # Run command received from args.
 $1
